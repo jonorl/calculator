12268e6bc38f5323ad08952647aa06a6f5d74b76
jonorl <39122230+jonorl@users.noreply.github.com>
2024-09-11 14:15:54 +0300

    First test of the calculator work!

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 
 
 const add = function(x, y) {
-	return x + y;
+	return +x + +y;
 };
 
 const subtract = function(x, y) {
-	return x - y;
+	return +x - +y;
 };
 
 const sum = function(array) {
@@ -18,6 +18,7 @@ const sum = function(array) {
 };
 
 const multiply = function(array) {
+
 
   let multiplied = array.reduce((accumulator, currentValue) => {
     return accumulator * currentValue},1
@@ -33,23 +34,58 @@ const divide = function(array) {
     return divided;
   };
 
+const operate = function(int1, mathOp, int2){
+  let arr = []
+  switch(mathOp){
+    case "÷": 
+      console.log(int1, int2); // Check input values
+      arr = [int1, int2]
+      console.log(arr); // Check array before reduce
+      return divide(arr);
+    case "x":
+      arr = [int1, int2]
+      return multiply(arr);
+    case "−":
+      return subtract(int1, int2);
+    case "+":
+      return add(int1, int2);
+  }
+}
+
+const equals = function(){
+  display.textContent = operate(firstNum, mathOperator, secondNum);
+  return operate(firstNum, mathOperator, secondNum);
+}
+
+
 // grab variables
 
 
 let numButtons = document.querySelectorAll(".oneToNine, .zero");
+let opButtons = document.querySelectorAll(".operator")
+let display = document.querySelector(".display");
 let firstNum = "";
 let secondNum = "";
+let mathOperator = "";
 let capturingFirst = true;
 
 numButtons.forEach(button => {
     button.addEventListener("click", function() {
         if (capturingFirst) {
-            firstNum = button.textContent;
-            capturingFirst = false; // Now capture the second number
+            firstNum += button.textContent;
         } else {
-            secondNum = button.textContent;
+            secondNum += button.textContent;
         }
-        console.log(firstNum);
-        console.log(secondNum);
     });
 });
+
+opButtons.forEach(button => {
+  button.addEventListener("click", function() {
+    mathOperator = button.textContent;
+    capturingFirst = false;
+  });
+});
+
+document.querySelector(".equals").addEventListener("click", equals);
+
+//test
